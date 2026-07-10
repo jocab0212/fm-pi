7bb7469ad40f4073d1e7cebb3f323a99683def7c
Jojo <jocabz01@gmail.com>
2026-07-10 17:03:21 +0800

    Fix homepage route

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <main style={{textAlign:"center", padding:"50px"}}>
-      <h1>🎵 FM.Pi Radio </h1>
+    <main style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🎵 FM.Pi Radio</h1>
       <p>Streaming built in Pi Network</p>
       <form>
         <input type="email" placeholder="Enter your email" />
         <button type="submit">Notify Me</button>
       </form>
     </main>
-  )
+  );
 }
