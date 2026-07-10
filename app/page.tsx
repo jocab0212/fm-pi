13bd7164edf836f6003058ca5c197c6753795e51
Jojo <jocabz01@gmail.com>
2026-07-10 16:14:42 +0800

    Trigger deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
     <main style={{textAlign:"center", padding:"50px"}}>
-      <h1>🎵 FM.Pi</h1>
-      <p>Streaming powered by Pi Network</p>
+      <h1>🎵 FM.Pi Radio </h1>
+      <p>Streaming built in Pi Network</p>
       <form>
         <input type="email" placeholder="Enter your email" />
         <button type="submit">Notify Me</button>
